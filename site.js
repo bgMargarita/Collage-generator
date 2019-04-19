7a79ee3643bfae874057c9b67cfe03d106163367
bgMargarita <margolyagavrilman@gmail.com>
2019-04-19 11:47:28 +0300

    add big div, change button remake makeCollage createDOM made image generation

--- site.js
+++ site.js
@@ -1,22 +1,41 @@
-function prepareCanvas() {
-    var body = document.getElementsByTagName('body')[0];
-    var x = document.createElement("canvas");
-    x.setAttribute('id', 'viewport');
-    document.body.appendChild(x);
+var coords = [];
+var texts = [];
 
-}
+function createDOM(width, height) {
+    var div = document.createElement('div')
+    document.body.appendChild(div);
 
-function refreshPage() {
-    var btn = document.createElement('input')
+    var div1 = document.createElement('div')
+    div1.id = 'div1';
+    div1.style.width = width;
+    div1.style.height = "40px";
+    div1.style.background = "#ededda";
+    div1.style.color = "white";
+
+    var btn = document.createElement('input');
     btn.id = 'b1';
     btn.type = 'button';
     btn.value = 'Update';
     btn.style = 'z-index:2';
     btn.onclick = function () {
-        window.location.reload();
+        makeCollage(width, height);
     };
-    document.body.appendChild(btn);
+    div1.appendChild(btn);
 
+    var div2 = document.createElement('div');
+    div2.id = 'div2';
+    div2.style.width = width;
+    div2.style.height = height;
+    div2.style.background = "#d7e1f2";
+    div2.style.color = "white";
+
+    var x = document.createElement("canvas");
+    x.setAttribute('id', 'viewport');
+    // document.body.appendChild(x);
+    div2.appendChild(x);
+    div.appendChild(div2);
+    div.appendChild(div1);
+//text
 }
 
 function makeCollage(width, height) {
@@ -26,15 +45,12 @@ function makeCollage(width, height) {
     canvas.height = height;
     context = canvas.getContext('2d');
 
-    createImage('https://images.unsplash.com/photo-1478905094053-75043450c873?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9'
-        , 0, 0, context);
-    createImage('https://images.unsplash.com/photo-1481747435017-5e320257af53?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&h=200&fit=crop&ixid=eyJhcHBfaWQiOjF9', 200, 0, context);
-    createImage('https://images.unsplash.com/photo-1453834190665-46ff0a1fbd5a?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&h=400&fit=crop&ixid=eyJhcHBfaWQiOjF9', 0, 300, context);
-    context.fillText("Hello world", 10, 50);
-    var maxWidth = 400; //размер поле, где выводится текст
-    context.font = "16pt Calibri";
-    context.fillStyle = "#000";
-    context.fillText("Hello !!", 50, 50, 400);
+    createImage('https://source.unsplash.com/collection/1127163/300x200', 0, 0, context, 0);
+    createImage('https://source.unsplash.com/collection/1127177/300x200', width / 2 + 20, 0, context, 1);
+    createImage('https://source.unsplash.com/collection/1127156/300x200', 0, height / 2, context, 2);
+    createImage('https://source.unsplash.com/collection/1147624/300x200', width / 2 + 20, height / 2, context, 3);
+
+
 }
 
 /*
@@ -58,43 +74,27 @@ function wrapText(context, text, marginLeft, marginTop, maxWidth, lineHeight) {
 
 */
 
-function createImage(url, y, x, context) {
+function createImage(url, y, x, context, index) {
     var base_image = new Image();
-    base_image.src = url;
 
-    base_image.onload = context.drawImage(base_image, x, y);
+    base_image.onload = function () {
+        context.drawImage(base_image, x, y);
+
+        context.font = "16pt Calibri";
+        context.fillStyle = "#000";
+        context.fillText(texts[index], x + 20, y + 50);
+    };
+
+    base_image.src = url;
 
 }
 
 $(document).ready(function () {
-    prepareCanvas();
-    makeCollage(900, 900);
-    $.ajax({
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'jsonp',
-        url: '',
-        success: function (jsondata) {
-
-        }
-    })
-
-    getPhrase();
-
-    refreshPage();
+    createDOM(500, 500);
 
 
 });
 
-function getPhrase() {
-    alert("getphrase");
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        //if (this.readyState == 4 && this.status == 200) {
-        alert(this.responseText);
-        //}
-    };
-    xhttp.open("GET", "https://api.forismatic.com/api/1.0/?method=getQuote", true);
-    //xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-}
+
+
+
